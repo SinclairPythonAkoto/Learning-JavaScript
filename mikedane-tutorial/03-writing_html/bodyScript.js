482ecb584a919c91d9ec311a7e1cbfb28350cb61
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-26 22:59:08 +0000

    index.html; headerScript.js; bodyScript.js files

---
+++ mikedane-tutorial/03-writing_html/bodyScript.js
@@ -0,0 +1,5 @@
+document.write("<h3 style='color:blue'>This is from the bodyScript.js file</h3>");
+
+document.write("<h1 style='color:green'>Hello World!</h1>");
+document.write("<hr>");
+document.write("<p>This is a javascript tutorial</p>");
---
+++ mikedane-tutorial/03-writing_html/headerScript.js
@@ -0,0 +1,11 @@
+alert("Hello World");
+document.write("<h2>Hello world!</h2>");
+document.write("<h3 style='color:red;'>This is from the headerScript.js file</h3>");
+
+document.write(".../|");
+document.write("<br>");
+document.write("../.|\n");
+document.write("<br>");
+document.write("./..|\n");
+document.write("<br>");
+document.write("/...|\n");
