4d69a8c57be220b8d25ae66195fbd70ffa8eecfb
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-26 23:37:14 +0000

    JavaScript file

---
+++ mikedane-tutorial/04-variables/script.js
@@ -0,0 +1,19 @@
+/*
+Below are a examples of some simple variables.  We will then combine them together in a print statment.
+We can also prodcue conditional logic with these statements, but that will be for later in the tutorial!
+*/
+
+var name = "Sinclair";				// string with double qoutes
+var occupation = 'developer';		// string with single qoutes
+
+var age = 35						// integer
+var height = 5.11					// float
+
+var isShort;						// boolean true/false
+var isTall;
+isShort = false;
+isTall = true;
+
+document.write("You name is " + name + "<br/>");
+document.write("Your age is: " + age + "<br/>");
+document.write("Your height: " + height + " ft <br/>");
