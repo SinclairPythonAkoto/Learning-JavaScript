188a38f54b885225aaa92969f924edc03607e8a6
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 05:36:53 +0100

    JavaScript file

--- mikedane-tutorial/06-math/script.js
+++ mikedane-tutorial/06-math/script.js
@@ -1,1 +1,23 @@
 alert("Computing math with JavaScript");
+
+
+document.write( 2 * 3 + "<br>" );       // Basic Arithmetic: +, -, /, *
+document.write( 2**3 + "<br>" );        // Exponents
+document.write( 10 % 3 + "<br>" );      // Modulus Op. : returns remainder of 10/3
+document.write( 1 + 2 * 3 + "<br>" );   // order of operations
+document.write(10 / 3.0 + "<br><br>");  // int's and doubles
+
+
+var num = 10;
+num += 100;                              // +=, -=, /=, *=
+document.write(num + "<br>");
+
+num++;                                   // this increments the value by 1
+document.write(num + "<br><br>");
+
+// Math class has useful math methods
+document.write( Math.pow(2, 3) + "<br>" );
+document.write( Math.sqrt(144) + "<br>" );
+document.write( Math.round(2.7) + "<br>" );
+
+
