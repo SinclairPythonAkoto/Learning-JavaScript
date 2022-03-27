20331cb8f852707ac9a47473d714b29c9052af81
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 11:59:19 +0100

    JavaScript file

--- mikedane-tutorial/10-functions/script.js
+++ mikedane-tutorial/10-functions/script.js
@@ -1,1 +1,45 @@
 alert("Creating functions with JavaScript!");
+
+/*
+Creating a simple function
+*/
+
+function hello(){
+    document.write("Hello");
+}
+
+hello();
+document.write("<br>");
+
+/*
+Function with 1x parameter
+*/
+
+function greeting(name){
+    document.write("Hello " + name);
+}
+
+greeting("Sinclair");
+document.write("<br>");
+
+/*
+Function with 2x parameters
+*/
+
+function sayHi(name, age){
+    document.write("Hello " + name + ", you are " + age);
+}
+
+sayHi("Sinclair", 35);
+document.write("<br>");
+
+/*
+Creating a function and putting it into a variable
+*/
+
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+var sum = addNumbers(4, 60);
+document.write(sum);
