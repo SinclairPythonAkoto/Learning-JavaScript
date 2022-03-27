8ab5e4b573c154c27a9ea96902bf777e91b61348
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 00:09:38 +0000

    JavaScript file

--- mikedane-tutorial/04-variables/script.js
+++ mikedane-tutorial/04-variables/script.js
@@ -3,6 +3,8 @@ Below are a examples of some simple variables.  We will then combine them togeth
 We can also prodcue conditional logic with these statements, but that will be for later in the tutorial!
 */
 
+alert("Creating Variables!");
+
 var name = "Sinclair";				// string with double qoutes
 var occupation = 'developer';		// string with single qoutes
 
@@ -16,4 +18,22 @@ isTall = true;
 
 document.write("You name is " + name + "<br/>");
 document.write("Your age is: " + age + "<br/>");
-document.write("Your height: " + height + " ft <br/>");
+document.write("Your height: " + height + " ft <br/><br/>");
+
+
+/*
+Now we will create variables for the example in the README.md.
+You will also see how we can change the value of the variables as we please.
+*/
+
+characterName = "George";
+characterAge = 70
+
+document.write("There once was a man named " + characterName + "<br/>");
+document.write("He was " + characterAge + " years old <br/>");
+document.write("He really liked the name " + characterName + "<br/>");
+document.write("But didn't like being " + characterAge + "<br/>");
+
+characterAge = 18
+
+document.write(characterName + " wished he was " + characterAge + " again! <br/>");
