3f29cccf1a7ea184bb461bea65ec011a88b772bf
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 04:47:25 +0100

    JavaScript file

--- mikedane-tutorial/05-strings/script.js
+++ mikedane-tutorial/05-strings/script.js
@@ -1,1 +1,22 @@
-alert("Creating Variables!");
+alert("Creating Strings");
+
+
+var greeting = "Hello";
+//   indexes:   01234
+
+var phrase = "Strings are cool";
+
+document.write( greeting.length + "<br>" );
+document.write( greeting.charAt(0) + "<br>"  );
+document.write( greeting.indexOf("llo") + "<br>"  );
+document.write( greeting.indexOf("z") + "<br>"  );
+document.write( greeting.substring(2) + "<br>"  );
+document.write( greeting.substring(1, 3) + "<br><br>"  );
+
+document.write( phrase.length + "<br>" );
+document.write( phrase.charAt(0) + "<br>"  );
+document.write( phrase.indexOf("o") + "<br>"  );
+document.write( phrase.indexOf("z") + "<br>"  );
+document.write( phrase.substring(8) + "<br>"  );
+document.write( phrase.substring(8, 11) + "<br><br>"  );
+
