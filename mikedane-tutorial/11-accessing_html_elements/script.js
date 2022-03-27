aac499e73cbce4b325b87b0b506689421e04b5ba
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 13:42:37 +0100

    Javascript file & index HTML file uploaded

--- mikedane-tutorial/11-accessing_html_elements/script.js
+++ mikedane-tutorial/11-accessing_html_elements/script.js
@@ -1,1 +1,7 @@
 alert("Accessing HTML Elements!");
+
+var header = document.getElementById("userName");
+header.style.color = "blue";
+
+document.write( header.getElementbyAttribute("nickname") );
+header.innerHTML = "Sinclair JavaScript Akoto";
