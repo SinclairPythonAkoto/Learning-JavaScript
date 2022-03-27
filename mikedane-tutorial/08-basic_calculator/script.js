8d32f96f3f55f6f339547e4c7d7e48d3e6d3de20
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-27 06:53:08 +0100

    JavaScript file

--- mikedane-tutorial/08-basic_calculator/script.js
+++ mikedane-tutorial/08-basic_calculator/script.js
@@ -1,2 +1,10 @@
 alert("Building a basic calculator with JavaScript!");
 alert("We will add two numbers together from a user input");
+
+var num1 = window.prompt("Number 1: ");
+var num2 = window.prompt("Number 2: ");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+alert( "Answer: " + (num1 + num2) );
