06db27875c70ee76a7e0341af35fa2a935663bc6
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-28 03:18:26 +0100

    JavaScript file uploaded; HTML file updated

--- mikedane-tutorial/13-messaging_app/script.js
+++ mikedane-tutorial/13-messaging_app/script.js
@@ -1,1 +1,14 @@
 alert("Building A Messaging App!");
+
+// create variables for the elements you want to interact with
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+// create event listener
+button.addEventListener("click", function(){
+    var newMessage = document.createElement("li");
+    newMessage.innerHTML = textbox.value;
+    message.appendChild(newMessage);
+    textbox.value = "";
+});
