5568a46a2fb4a53d9e9e19addb5d27431dec8210
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-29 07:21:56 +0100

    JavaScript file

--- mikedane-tutorial/16-switch_statements/script.js
+++ mikedane-tutorial/16-switch_statements/script.js
@@ -1,1 +1,50 @@
 alert("Creatign Switch Statements");
+
+/*
+   We will create a function that will use a swtich statement to calculate 
+   the day of the week when we pass in a number from 1 - 7.
+ */
+
+// 1 -> Monday
+// 2 -> Tuesday
+// ...
+// 6 -> Saturday
+// 7 -> Sunday
+
+
+function getDayName(dayNum){
+	var day;
+
+	switch(dayNum){
+		case 1:
+			day = "Monday";
+			break
+		case 2:
+			day = "Tuesday";
+			break
+		case 3:
+			day = "Wednesday";
+			break
+		case 4:
+			day = "Thursday";
+			break
+		case 5:
+			day = "Friday";
+			break
+		case 6:
+			day = "Saturday";
+			break
+		case 7:
+			day = "Sunday";
+			break
+		default:
+			day = "Day does not exist. " + dayNum + " is not a valid number.";
+	}
+
+	return day;
+}
+
+/*
+Change the values to see what you get back
+*/
+document.write(getDayName(4));
