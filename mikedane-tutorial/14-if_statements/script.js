0811e4f07d00407349e726b5f7eebdbd6bb55cc3
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-29 05:59:16 +0100

    JavaScript file

--- mikedane-tutorial/14-if_statements/script.js
+++ mikedane-tutorial/14-if_statements/script.js
@@ -1,1 +1,18 @@
 alert("Creating If Statements!");
+
+/*
+Change the value of these variables to false to see the different responses you get.
+*/
+
+var isMale = true;
+var isTall = true;
+
+if(isMale && isTall){
+    document.write("You are a tall male");
+} else if(isMale && !isTall){
+    document.write("You are a short male");
+} else if(!isMale && isTall){
+    document.write("you are not male, but you are tall");
+} else {
+    document.write("You are not tall and not male");
+}
