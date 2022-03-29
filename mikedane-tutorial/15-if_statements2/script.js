ef0cea326b5378a0f1f3d3f67918f119f822c71d
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-29 06:36:37 +0100

    JavaScript file

--- mikedane-tutorial/15-if_statements2/script.js
+++ mikedane-tutorial/15-if_statements2/script.js
@@ -1,1 +1,13 @@
 alert("Using Comparison Operators With If Statements!");
+
+function max(num1, num2, num3){
+    if(num1 >= num2 && num1 && num3){
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3){
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+document.write("The largest number is: " + max(30, 10, 5));
