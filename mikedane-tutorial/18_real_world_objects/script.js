7458571a9c0528a76f639b1619f7e10f3681ee48
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-30 05:44:32 +0100

    JavaScript file

--- mikedane-tutorial/18_real_world_objects/script.js
+++ mikedane-tutorial/18_real_world_objects/script.js
@@ -1,1 +1,31 @@
 alert("Creating Real World Objects With JavaScript!");
+
+var myMovie = {
+     title: "The Social Network",
+     releaseYear: "2010",
+     duration: 2.0,
+     actors: [
+          {
+               name: "Jessie Eisenberg",
+               birthday: new Date("October 5, 1983"),
+               hometown: "New York, New York"
+          },
+          {
+               name: "Roonie Mara",
+               birthday: new Date("April 17, 1985"),
+               hometown: "Bedford New York"
+          }
+     ]
+};
+
+// accessing individual actors
+document.write("Actor's names: <br>");
+document.write( myMovie.actors[0].name + "<br>" );
+document.write(myMovie.actors[1].name + "<br>" );
+
+document.write("<br>");
+
+// accessing invidual hometowns
+document.write("Actor's hometown: <br>");
+document.write( myMovie.actors[0].hometown + "<br>" );
+document.write( myMovie.actors[1].hometown + "<br>" );
