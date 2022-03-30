563aa802fdcdae56d1d9dda5bd0213b8b32aa7de
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-30 04:22:09 +0100

    JavaScript file

--- mikedane-tutorial/17-objects/script.js
+++ mikedane-tutorial/17-objects/script.js
@@ -1,1 +1,30 @@
 alert("JavaScript Objects!");
+
+// creating an object
+var person = {
+    firstName: "Sinclair",
+    lastName: "Akoto",
+    age: 35,
+    height: 5.11,
+    isMale: true,
+    occupation: "software developer",
+    company: "Scalian"
+};
+
+// accessing data within objects
+document.write( 
+    "My name is " + person.firstName + " " + person.lastName + ". I am " + person.age + " years old, and work as a " + person.occupation + " for " + person.company + ". <br>"
+);
+
+// chnaging the values of an object
+person.firstName = "Jane"
+person.lastName = "Doe"
+person.age = 32
+person.isMale = false
+person.occupation = "Senior Buisness Manager"
+person.company = "some tech company"
+
+document.write( 
+    "My name is " + person.firstName + " " + person.lastName + ". I am " + person.age + " years old, and work as a " + person.occupation + " for " + person.company + ". <br>"
+);
+
