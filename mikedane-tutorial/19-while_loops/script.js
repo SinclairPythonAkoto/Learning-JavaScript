0bfa06de1971ade5e76ee496ce2aaa576a95c827
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-30 06:14:54 +0100

    JavaScript file

--- mikedane-tutorial/19-while_loops/script.js
+++ mikedane-tutorial/19-while_loops/script.js
@@ -1,1 +1,24 @@
 alert("While Loops & Do While Loops!");
+
+// creating a while loop
+document.write("Creating a while loop: <br>");
+
+index = 1;
+
+while(index <= 5){
+    document.write(index + "<br>");
+    index++;
+}
+
+document.write("<br><br>");
+
+// creating a do loop
+document.write("Creating a do loop: <br>");
+
+index = 1;
+
+do{
+  document.write(index + "<br>");
+  index++;
+}while(index <= 5);
+
