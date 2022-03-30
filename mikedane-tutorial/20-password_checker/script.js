09913004e7d7b79c02d2c950fc6e6b5e80e350e7
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-30 06:27:47 +0100

    JavaScript file

--- mikedane-tutorial/20-password_checker/script.js
+++ mikedane-tutorial/20-password_checker/script.js
@@ -1,1 +1,27 @@
-alert("Building A PAssword Checker With JavaScript!)");
+alert("Building A PAssword Checker With JavaScript!");
+
+/*
+You can change the password to whatever you like!
+*/
+
+var password = "wordpass";
+var response;
+var entryCount = 0;
+var entryLimit = 3;
+var error = false;
+
+while(response != password && !error){
+     if(entryCount < entryLimit){
+          response = window.prompt("Enter Password");
+          entryCount++;
+     } else {
+          error = true;
+     }
+}
+
+if(error){
+     alert("Too many entries");
+} else {
+     alert("Success");
+}
+
