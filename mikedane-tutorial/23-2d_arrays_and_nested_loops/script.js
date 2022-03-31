ec544db3f89747ad42bc1a93ae4cf557678eaca0
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-31 07:58:00 +0100

    JavaScript file

--- mikedane-tutorial/23-2d_arrays_and_nested_loops/script.js
+++ mikedane-tutorial/23-2d_arrays_and_nested_loops/script.js
@@ -1,2 +1,21 @@
 alert("2D Arrays & Nested Loops in JavaScript");
 
+// 2d array
+numberGrid = [ [1, 2], [3, 4] ];
+
+// modifying a value
+numberGrid[0][1] = 99;
+
+// getting data from an 2d array
+document.write(numberGrid[0][0] + "<br>");
+document.write(numberGrid[0][1] + "<br>");
+
+// nested loops
+for(var i = 0; i < numberGrid.length; i++){
+     for(var j = 0; j < numberGrid[i].length; j++){
+          document.write(numberGrid[i][j] + ", ");
+     }
+     document.write("<br>");
+}
+
+
