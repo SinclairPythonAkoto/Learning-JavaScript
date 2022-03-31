cd8fcce35df1165ab67c31ea82a49594231179fd
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-31 06:43:12 +0100

    JavaScript file

--- mikedane-tutorial/21-for_loops/script.js
+++ mikedane-tutorial/21-for_loops/script.js
@@ -1,1 +1,26 @@
 alert("Creating For Loops!");
+
+
+for(var i = 0; i < 5; i++){
+     document.write(i + "<br>");
+}
+document.write("<br>");
+
+
+/* 
+iterating through a list
+*/
+var luckyNums = [4, 8, 15, 16, 23, 42];
+luckyNums.forEach(function(luckyNum){
+     document.write(luckyNum + "<br>");
+});
+document.write("<br>");
+
+
+/*
+interating through a list
+*/
+var friends = ["Jim", "John", "Jane", "Jason", "Jack", "Joseph", "Juliet"]
+for(var index = 0; index < friends.length; index++){
+	document.write(friends[index] + "<br>");
+}
