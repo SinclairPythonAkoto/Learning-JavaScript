9ada7954f38ecfd150e8b9c7b9e9a5dab5cf4447
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-04-01 08:35:12 +0100

    JavaScript file

--- mikedane-tutorial/26-web_storage/script.js
+++ mikedane-tutorial/26-web_storage/script.js
@@ -1,1 +1,10 @@
 alert("Web Storage With JavaScript!");
+
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("btn");
+
+button.addEventListener("click", function(){
+    localStorage.name = textbox.value;
+});
+
+document.write(localStorage.name);
