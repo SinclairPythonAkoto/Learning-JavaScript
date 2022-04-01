3766f333ba59ab935ea06858d11074b41f625c11
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-04-01 08:05:09 +0100

    JavaScript file

--- mikedane-tutorial/25-timing/script.js
+++ mikedane-tutorial/25-timing/script.js
@@ -1,1 +1,11 @@
 alert("Timing Functions!");
+
+// set time out function 
+function helloWorld(){
+    alert("Hello World!");
+}
+
+// set interval
+function sayhI(){
+    alert("Hello user, See you again in 5 seconds!");
+}
